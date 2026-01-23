314ed9c0b4e24004fc3077b70b03a55232fb485e
datorres335 <datorres335@gmail.com>
2026-01-22 21:30:19 -0800

    finished up to exercise 3.11

--- part3/frontend/src/services/persons.js
+++ part3/frontend/src/services/persons.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 //const baseUrl = 'http://localhost:3001/api/persons'
 const baseUrl = '/api/persons' //need to use relative URL when you copy 'dist' (aka frontend production build, aka 'npm run build') into backend dir
+  // your going to have to use a proxy in the backend to make the frontend work again in dev mode
+  // proxy added in vite.config.js
 
 const getAll = () => {
   const request = axios.get(baseUrl)
