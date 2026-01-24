33639145744448ca2a9a0afd9252bdb28aa6a763
datorres335 <datorres335@gmail.com>
2026-01-23 18:04:40 -0800

    finished exercises up to 3.18

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
+//app.use(requestLogger) // not sure what this is but is causing app to fail 1/23/26 6:02pm
+
 const morgan = require('morgan')
 const Person = require('./models/person')
-
-app.use(express.json())
-app.use(express.static('dist'))
 
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -59,7 +60,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   // const id = request.params.id
   // const person = persons.find(person => person.id === id)
 
@@ -71,16 +72,30 @@ app.get('/api/persons/:id', (request, response) => {
 
   Person.findById(request.params.id)
     .then(person => {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-    .catch(error => response.status(404).end())
+    .catch(error => {
+      console.log('Error in finding person: ', error)
+      next(error)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+  // const id = request.params.id
+  // persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+  // response.status(204).end()
+
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(`${result} was deleted`)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', async (request, response) => {
@@ -119,6 +134,21 @@ app.post('/api/persons', async (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) return response.status(404).end()
+
+      person.name = name
+      person.number = number
+
+      return person.save().then(updatedPerson => response.json(updatedPerson))
+    })
+    .catch(error => next(error))
+})
+
 app.get('/info', (request, response) => {
   const numOfPeople = persons.length
   const now = new Date().toString()
@@ -131,6 +161,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT // process.env.PORT is necessary to deploy app on Render.com
 app.listen(PORT, () => {
