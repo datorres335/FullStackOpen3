4678ef1693d6a811f9698d5bce4dd590cd6905c3
datorres335 <datorres335@gmail.com>
2026-01-26 17:34:34 -0800

    finished exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const sum = (total, blog) => total + blog.likes
+
+  return blogs.reduce(sum, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
