fd4d250ca6f10165ea3449dc38e7c7c5d5839ca7
datorres335 <datorres335@gmail.com>
2026-01-27 16:00:13 -0800

    created test to check if a valid blog can be added

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,40 +1,42 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
     response.json(blogs)
-  })
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) response.json(blog)
+    else response.status(404).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,9 +1,41 @@
 const assert = require('node:assert')
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "No Title Test 1",
+    author: "No Author Test 1",
+    url: "No URL",
+    likes: 0
+  },
+  {
+    title: "No Title Test 2",
+    author: "No Author Test 2",
+    url: "No URL",
+    likes: 1
+  },
+  {
+    title: "No Title Test 3",
+    author: "No Author Test 3",
+    url: "No URL",
+    likes: 2
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -14,13 +46,34 @@ test('blogs are returned as json', async () => {
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 3)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
   const contents = response.body.map(e => e.title)
   assert.strictEqual(contents.includes('No Title Test 1'), true)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Blog from test file",
+    author: "author Test",
+    url: "none",
+    likes: 999
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+  assert(contents.includes("Blog from test file"))
 })
 
 after(async () => {
