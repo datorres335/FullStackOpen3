c58e7014bf1c67d1682ec0740fe940629f9aa44c
datorres335 <datorres335@gmail.com>
2026-01-28 17:19:56 -0800

    finished up to exercise 4.23; fixed problem from exercise 4.20 (now working as should)

--- part4/models/user.js
+++ part4/models/user.js
@@ -17,14 +17,14 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
-userSchema.set('toJSON'), {
+userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
     delete returnedObject.passwordHash
   }
-}
+})
 
 const User = mongoose.model('User', userSchema)
 
--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -3,23 +3,53 @@ const User = require('../models/user')
 
 const initialBlogs = [
   {
-    title: "No Title Test 1",
-    author: "No Author Test 1",
-    url: "No URL",
-    likes: 0
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
   },
   {
-    title: "No Title Test 2",
-    author: "No Author Test 2",
-    url: "No URL",
-    likes: 1
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
   },
   {
-    title: "No Title Test 3",
-    author: "No Author Test 3",
-    url: "No URL",
-    likes: 2
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
   },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
 ]
 
 const nonExistingId = async () => {
