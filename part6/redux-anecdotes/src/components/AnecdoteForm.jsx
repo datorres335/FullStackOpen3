5e7badaea0698b4c4d315e2a70bbeebdd6cb73eb
datorres335 <datorres335@gmail.com>
2026-02-02 20:58:22 -0800

    finished up to exercise 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux"
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const onCreate = event => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+
+    dispatch(createAnecdote({ content, votes: 0 }))
+    dispatch(setNotification(`anecodote ${content} voted`, 5))
+  }
+
+  return (
+    <div>
+      <h2>Create New</h2>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
