00737f08d04b74c9ab6dc7b634ba455ff84f51fb
datorres335 <datorres335@gmail.com>
2026-02-09 22:23:11 -0800

    finished up to /blogapp/frontend/src/components/Blog.js

--- part7/blogapp/redux-frontend/src/App.jsx
+++ part7/blogapp/redux-frontend/src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { Routes, Route, Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import LoginForm from './components/Login'
+import NewBlog from './components/NewBlog'
+import Notification from './components/Notification'
+import Users from './components/Users'
+import User from './components/User'
+import Blogs from './components/Blogs'
+import Blog from './components/Blog'
+import Togglable from './components/Togglable'
+
+import { useNotification, useInitialization, useClearUser } from './hooks/index'
+
+import { SmallButton, Page, Navigation } from './components/styled'
+
+const App = () => {
+
+  const blogFormRef = useRef()
+  const stateInitializer = useInitialization()
+  const notifyWith = useNotification()
+
+  const clearUser = useClearUser()
+
+  const user = useSelector(({ user }) => user)
+
+  useEffect(() => {
+    stateInitializer()
+  }, [])
+
+  const logout = async () => {
+    clearUser()
+    notifyWith('logged out')
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <Notification />
+        <LoginForm />
+      </div>
+    )
+  }
+
+  const padding = {
+    padding: 5
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Page>
+
+      <Navigation>
+        <span style={padding}><strong>Blog app</strong></span>
+        <Link style={padding} to="/">blogs</Link>
+        <Link style={padding} to="/users">users</Link>
+        <span style={padding}>{user.name} logged in</span>
+        <span style={padding}>
+          <SmallButton onClick={logout}>logout</SmallButton>
+        </span>
+      </Navigation>
+
+      <Notification />
+
+      <Routes>
+        <Route path="/users" element={ <Users />}></Route>
+        <Route path="/users/:id" element={ <User />}></Route>
+        <Route path="/blogs/:id" element={ <Blog />}></Route>
+        <Route path="/" element={ <Blogs />}></Route>
+      </Routes>
+
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <NewBlog hideMe={() => blogFormRef.current.toggleVisibility()} />
+      </Togglable>
+    </Page>
   )
 }
 
