907c3f9f27a56c18881eef4a0958993214cf71a0
datorres335 <datorres335@gmail.com>
2026-02-10 21:45:22 -0800

    finished up to /blogapp/frontend-react-query/src/services/users.js

--- part7/blogapp/react-query-frontend/src/main.jsx
+++ part7/blogapp/react-query-frontend/src/main.jsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+import { NotificationContextProvider } from './contexts/notification'
+import { UserContextProvider } from './contexts/user'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <NotificationContextProvider>
+          <App />
+        </NotificationContextProvider>
+      </UserContextProvider>
+    </QueryClientProvider>
+  </Router>
 )
