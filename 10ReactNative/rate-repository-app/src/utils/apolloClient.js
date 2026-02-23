4ce6bd48bd1989c852081c9ffbfe50b477b02fa7
datorres335 <datorres335@gmail.com>
2026-02-22 19:53:04 -0800

    finished up to exercise 10.12

--- 10ReactNative/rate-repository-app/src/utils/apolloClient.js
+++ 10ReactNative/rate-repository-app/src/utils/apolloClient.js
@@ -1,9 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
+
+const { apolloUri } = Constants.expoConfig.extra;
 
 //NOTE: you find the correct URL value once you start the app with "npm start" under "Metro waiting on exp://192.168.1.233:8081" (the stuff between exp:// and :8081)
 const createApolloClient = () => {
   return new ApolloClient({
-    link: new HttpLink({ uri: 'http://192.168.1.233:4000/graphql' }),
+    link: new HttpLink({ uri: apolloUri }),
     cache: new InMemoryCache(),
   })
 };
