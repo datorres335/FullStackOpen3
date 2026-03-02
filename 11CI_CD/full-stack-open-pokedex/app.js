0da8191ee6d5755100d7646827cf8f69a0bfeb88
datorres335 <datorres335@gmail.com>
2026-03-01 16:03:32 -0800

    working on exercise 11.13

--- 11CI_CD/full-stack-open-pokedex/app.js
+++ 11CI_CD/full-stack-open-pokedex/app.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
+  // UNCOMMNET TO SIMULATE A BROKEN DEPLOYMENT, NEED TO IMPLEMENT A TEST FOR THIS THO AND ADD TO pipeline.yml WORKFLOW
+  // // eslint-disable-next-line no-constant-condition
+  // if (true) throw('error...  ')
   res.send('ok')
 })
 
