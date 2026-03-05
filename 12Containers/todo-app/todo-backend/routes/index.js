43a0ed6b80870c1cdc05a1a6c99b8df9143b6708
datorres335 <datorres335@gmail.com>
2026-03-04 20:38:40 -0800

    finished exercise 12.11

--- 12Containers/todo-app/todo-backend/routes/index.js
+++ 12Containers/todo-app/todo-backend/routes/index.js
@@ -16,4 +16,12 @@ router.get('/', async (req, res) => {
   });
 });
 
+router.get('/statistics', async (req, res) => {
+  const added_todos = await redis.getAsync('created_todos') || 0
+
+  res.send({
+    added_todos: parseInt(added_todos)
+  });
+});
+
 module.exports = router;
