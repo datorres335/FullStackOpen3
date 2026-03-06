f9d1790da152a66a73edb3d1c0c0c6d9c400c1b4
datorres335 <datorres335@gmail.com>
2026-03-05 17:55:04 -0800

    finished up to exercise 12.14

--- 12Containers/todo-app/todo-frontend/.eslintrc.cjs
+++ 12Containers/todo-app/todo-frontend/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  globals: { test: 'readonly', expect: 'readonly', describe: 'readonly', beforeEach: 'readonly', afterEach: 'readonly', vi: 'readonly' },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
